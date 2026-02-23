f170d7a5f815025cfc6c444107c374793af544dd
PrathameshNithyanandan <prathamesh1524@gmail.com>
2026-02-23 09:14:54 +0530

    ccccc#

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,37 @@
+
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+    public static boolean isPalindrome(String content) {
+        content = content.replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = content.length() - 1;
+
+        while (left < right) {
+            if (content.charAt(left) != content.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+
 
 }
