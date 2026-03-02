46b3ceb060b42ccd24e00116c5d52c01a09c0d38
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-02 09:51:41 +0530

    UC7
    Implement a Deque-based palindrome checker with user input.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,31 +1,55 @@
-import java.util.*;
 
-class UseCase10PalindromeCheckerApp
-{
- public static void main(String[] args)
- {
-  Scanner sc=new Scanner(System.in);
-  System.out.println("enter string");
-  String s=sc.nextLine();
-
-  s=s.replaceAll("\\s+","").toLowerCase();
-
-  int i=0;
-  int j=s.length()-1;
-  boolean ok=true;
-
-  while(i<j)
-  {
-   if(s.charAt(i)!=s.charAt(j))
-   {
-    ok=false;
-    break;
-   }
-   i++;
-   j--;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+  public static boolean isPal(String str) {
+  
+      if (str == null) {
+          return false;
+      }
+  
+      String s = str.replaceAll("\\s+", "").toLowerCase();
+  
+      Deque<Character> dq = new ArrayDeque<Character>();
+  
+      for (int i = 0; i < s.length(); i++) {
+          char c = s.charAt(i);
+          dq.addLast(c);
+      }
+  
+      boolean flag = true;
+  
+      while (dq.size() > 1) {
+  
+          char f = dq.removeFirst();
+          char r = dq.removeLast();
+  
+          if (f != r) {
+              flag = false;
+              break;
+          }
+      }
+  
+      return flag;
   }
-
-  if(ok) System.out.println("palindrome");
-  else System.out.println("not palindrome");
- }
-}
+  
+  public static void main(String[] args) {
+  
+      Scanner sc = new Scanner(System.in);
+  
+      System.out.println("==== UC7: Deque-Based Palindrome Checker ====");
+      System.out.print("Enter a word or phrase: ");
+  
+      String input = sc.nextLine();
+  
+      boolean res = isPal(input);
+  
+      if (res == true) {
+          System.out.println("Result: The given input IS a Palindrome.");
+      } else {
+          System.out.println("Result: The given input is NOT a Palindrome.");
+      }
+  
+      sc.close();
+  }
