319249fcdfd2cbf1805cdbb72213cc2deefe2d81
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-02 10:03:52 +0530

    UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,33 @@
-public class PalindromeCheckerApp {
-
+import java.util.*;
+public class UseCase11PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input : ");
+        String s = "";
+        if (sc.hasNextLine()) s = sc.nextLine();
+        PalindromeService p = new PalindromeService();
+        System.out.println("is palindrome? : " + p.checkPalindrome(s));
+        sc.close();
+    }
+}
+class PalindromeService {
+    public boolean checkPalindrome(String input) {
+        if (input == null) return false;
+        int start = 0, end = input.length() - 1;
+        while (start < end) {
+            char a = input.charAt(start), b = input.charAt(end);
+            while (start < end && !Character.isLetterOrDigit(a)) {
+                start++;
+                a = input.charAt(start);
+            }
+            while (start < end && !Character.isLetterOrDigit(b)) {
+                end--;
+                b = input.charAt(end);
+            }
+            if (Character.toLowerCase(a) != Character.toLowerCase(b)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
