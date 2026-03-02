8a47d2d1517daf788096b5c6839654a71bd240ce
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-02 09:33:16 +0530

    UC9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,23 @@
-public class PalindromeCheckerApp {
 
+import java.util.*;
+
+class PalindromeCheckerApp
+{
+ static boolean check(String s,int start,int end)
+ {
+  if(start>=end) return true;
+  if(s.charAt(start)!=s.charAt(end)) return false;
+  return check(s,start+1,end-1);
+ }
+
+ public static void main(String[] args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter string");
+  String s=sc.nextLine();
+  s=s.replaceAll("\\s+","").toLowerCase();
+
+  if(check(s,0,s.length()-1)) System.out.println("palindrome");
+  else System.out.println("not palindrome");
+ }
 }
