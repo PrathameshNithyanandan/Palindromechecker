eb8842cda7c803616b2e17c0d2b1e4f53d53a45a
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-02 09:38:10 +0530

    UC10

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,31 @@
-public class PalindromeCheckerApp {
+import java.util.*;
 
+class UseCase10PalindromeCheckerApp
+{
+ public static void main(String[] args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter string");
+  String s=sc.nextLine();
+
+  s=s.replaceAll("\\s+","").toLowerCase();
+
+  int i=0;
+  int j=s.length()-1;
+  boolean ok=true;
+
+  while(i<j)
+  {
+   if(s.charAt(i)!=s.charAt(j))
+   {
+    ok=false;
+    break;
+   }
+   i++;
+   j--;
+  }
+
+  if(ok) System.out.println("palindrome");
+  else System.out.println("not palindrome");
+ }
 }
