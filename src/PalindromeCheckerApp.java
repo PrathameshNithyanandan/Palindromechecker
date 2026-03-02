389d6120be6ee78f6a29654ee0ad6bd30d8b944a
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-02 09:49:21 +0530

    UC7'
    Implement a Deque-based palindrome checker with user input.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,55 @@
-public class PalindromeCheckerApp {
 
-}
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+  public static boolean isPal(String str) {
+  
+      if (str == null) {
+          return false;
+      }
+  
+      String s = str.replaceAll("\\s+", "").toLowerCase();
+  
+      Deque<Character> dq = new ArrayDeque<Character>();
+  
+      for (int i = 0; i < s.length(); i++) {
+          char c = s.charAt(i);
+          dq.addLast(c);
+      }
+  
+      boolean flag = true;
+  
+      while (dq.size() > 1) {
+  
+          char f = dq.removeFirst();
+          char r = dq.removeLast();
+  
+          if (f != r) {
+              flag = false;
+              break;
+          }
+      }
+  
+      return flag;
+  }
+  
+  public static void main(String[] args) {
+  
+      Scanner sc = new Scanner(System.in);
+  
+      System.out.println("==== UC7: Deque-Based Palindrome Checker ====");
+      System.out.print("Enter a word or phrase: ");
+  
+      String input = sc.nextLine();
+  
+      boolean res = isPal(input);
+  
+      if (res == true) {
+          System.out.println("Result: The given input IS a Palindrome.");
+      } else {
+          System.out.println("Result: The given input is NOT a Palindrome.");
+      }
+  
+      sc.close();
+  }
