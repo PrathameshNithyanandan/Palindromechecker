2d3bfc026981f5ba2cd62da8459dde94a89aae9d
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-02 10:08:00 +0530

    UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,55 +1,33 @@
-
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
-public class PalindromeCheckerApp {
-  public static boolean isPal(String str) {
-  
-      if (str == null) {
-          return false;
-      }
-  
-      String s = str.replaceAll("\\s+", "").toLowerCase();
-  
-      Deque<Character> dq = new ArrayDeque<Character>();
-  
-      for (int i = 0; i < s.length(); i++) {
-          char c = s.charAt(i);
-          dq.addLast(c);
-      }
-  
-      boolean flag = true;
-  
-      while (dq.size() > 1) {
-  
-          char f = dq.removeFirst();
-          char r = dq.removeLast();
-  
-          if (f != r) {
-              flag = false;
-              break;
-          }
-      }
-  
-      return flag;
-  }
-  
-  public static void main(String[] args) {
-  
-      Scanner sc = new Scanner(System.in);
-  
-      System.out.println("==== UC7: Deque-Based Palindrome Checker ====");
-      System.out.print("Enter a word or phrase: ");
-  
-      String input = sc.nextLine();
-  
-      boolean res = isPal(input);
-  
-      if (res == true) {
-          System.out.println("Result: The given input IS a Palindrome.");
-      } else {
-          System.out.println("Result: The given input is NOT a Palindrome.");
-      }
-  
-      sc.close();
-  }
+import java.util.*;
+public class UseCase11PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input : ");
+        String s = "";
+        if (sc.hasNextLine()) s = sc.nextLine();
+        PalindromeService p = new PalindromeService();
+        System.out.println("is palindrome? : " + p.checkPalindrome(s));
+        sc.close();
+    }
+}
+class PalindromeService {
+    public boolean checkPalindrome(String input) {
+        if (input == null) return false;
+        int start = 0, end = input.length() - 1;
+        while (start < end) {
+            char a = input.charAt(start), b = input.charAt(end);
+            while (start < end && !Character.isLetterOrDigit(a)) {
+                start++;
+                a = input.charAt(start);
+            }
+            while (start < end && !Character.isLetterOrDigit(b)) {
+                end--;
+                b = input.charAt(end);
+            }
+            if (Character.toLowerCase(a) != Character.toLowerCase(b)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
