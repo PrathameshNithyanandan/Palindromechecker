8f01f766aaf8efe67ec8179be61af595609f01ab
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-02 09:34:22 +0530

    UC9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,69 +1,13 @@
+
 import java.util.*;
 
 class PalindromeCheckerApp
 {
- static class Node
+ static boolean check(String s,int start,int end)
  {
-  char data;
-  Node next;
-  Node(char d)
-  {
-   data=d;
-   next=null;
-  }
- }
-
- static Node head=null;
-
- static void add(char c)
- {
-  Node n=new Node(c);
-  if(head==null)
-  {
-   head=n;
-   return;
-  }
-  Node t=head;
-  while(t.next!=null) t=t.next;
-  t.next=n;
- }
-
- static boolean check()
- {
-  if(head==null||head.next==null) return true;
-
-  Node slow=head;
-  Node fast=head;
-
-  while(fast!=null && fast.next!=null)
-  {
-   slow=slow.next;
-   fast=fast.next.next;
-  }
-
-  Node prev=null;
-  Node curr=slow;
-  Node next=null;
-
-  while(curr!=null)
-  {
-   next=curr.next;
-   curr.next=prev;
-   prev=curr;
-   curr=next;
-  }
-
-  Node first=head;
-  Node second=prev;
-
-  while(second!=null)
-  {
-   if(first.data!=second.data) return false;
-   first=first.next;
-   second=second.next;
-  }
-
-  return true;
+  if(start>=end) return true;
+  if(s.charAt(start)!=s.charAt(end)) return false;
+  return check(s,start+1,end-1);
  }
 
  public static void main(String[] args)
@@ -71,12 +15,9 @@ class PalindromeCheckerApp
   Scanner sc=new Scanner(System.in);
   System.out.println("enter string");
   String s=sc.nextLine();
-
   s=s.replaceAll("\\s+","").toLowerCase();
 
-  for(int i=0;i<s.length();i++) add(s.charAt(i));
-
-  if(check()) System.out.println("palindrome");
+  if(check(s,0,s.length()-1)) System.out.println("palindrome");
   else System.out.println("not palindrome");
  }
 }
